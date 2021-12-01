f869c46f944d27e15c6791150602f4c821d234c1
luneShao <lunesir@gmail.com>
2021-12-01 16:10:30 +0800

    test: router

---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const routes = [
+  {
+    path: '/',
+    redirect: 'home',
+  },
+  {
+    path: '/home',
+    component: () => import ('@/views/Home.vue')
+  },
+  {
+    path: '/about',
+    component: () => import ('@/views/About.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
---
+++ src/views/Home.vue
@@ -0,0 +1,9 @@
+<script setup>
+</script>
+
+<template>
+  <div>Home page</div>
+</template>
+
+<style scoped>
+</style>
